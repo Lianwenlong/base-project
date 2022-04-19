26cb8241b9574d74b604ed5df1bf4a13eae5699d
lianwl <Lianwenlong@keendata.com>
2022-04-19 09:51:28 +0800

    base project template

---
+++ base-web/src/main/java/com/lian/base/web/Application.java
@@ -0,0 +1,22 @@
+package com.lian.base.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author LianWenLong
+ * @version 1.0
+ * @ClassName Application
+ * @Description
+ * @createTime 2022年04月18日 18:05
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
